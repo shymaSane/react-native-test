77ab2d3a64d57a68260c58c496f3ad1fba5f8242
shymaSane <shymaSane1@gmail.com>
2021-09-22 21:02:15 +0300

    [fix]scrollView height

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,50 +1,102 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, Image} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import styles from '../styling/styles';
 import UserOrientations from '../assets/UserOrientations';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Signup = ({navigation}) => {
   const WIDTH = UserOrientations().width;
   const HEIGHT = UserOrientations().height;
 
   return (
-    <View style={{width: .85*WIDTH ,...styles.signupContainer}}>
-      {/* logo section */}
-      <View style={styles.signupS1Container}>
-        <Image
-          style={styles.signupLogo}
-          source={require('../assets/images/logo-short.png')}
-        />
-        <Text style={styles.signupLogoText}>Where Stories Live</Text>
+    <View style={styles.signupContainer}>
+      <View style={{width: WIDTH*.85}}>
+        <ScrollView style={{flexGrow:1}}>
+          {/* logo section */}
+          <View style={styles.signupS1Container}>
+            <Image
+              style={styles.signupLogo}
+              source={require('../assets/images/logo-short.png')}
+            />
+            <Text style={styles.signupLogoText}>Where Stories Live</Text>
+          </View>
+          {/* sign up with fb & google section */}
+          <View style={styles.signupS2Container}>
+            <TouchableOpacity style={styles.facebookSignupButton}>
+              <Image
+                style={styles.signupFacebookLogo}
+                source={require('../assets/images/facebook.png')}
+              />
+              <Text style={styles.SignupS2Text}>Facebook</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.googleSignupButton}>
+              <Image
+                style={styles.signupGoogleLogo}
+                source={require('../assets/images/google.png')}
+              />
+              <Text style={styles.SignupS2Text}>Google</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.emptyViewContainer}>
+            <View style={styles.emptyView}></View>
+            <Text style={styles.emptyViewText}>or</Text>
+            <View style={styles.emptyView}></View>
+          </View>
+          {/* signup form section/email, username, password, birthday */}
+          <View style={styles.inputsContainer}>
+            <View style={styles.inputContainer}>
+              <Image
+                style={styles.inputImage}
+                source={require('../assets/images/email.png')}
+              />
+              <TextInput style={styles.inputStyle} placeholder="Email" />
+            </View>
+            <View style={styles.inputContainer}>
+              <Image
+                style={styles.inputImage}
+                source={require('../assets/images/user.png')}
+              />
+              <TextInput style={styles.inputStyle} placeholder="Username" />
+            </View>
+            <View style={styles.inputContainer}>
+              <Image
+                style={styles.inputImage}
+                source={require('../assets/images/lock.png')}
+              />
+              <TextInput style={styles.inputStyle} placeholder="Password" />
+              <Image
+                style={styles.inputImage}
+                source={require('../assets/images/unlock.png')}
+              />
+            </View>
+            <View style={styles.inputContainer1}>
+              <Image
+                style={styles.inputImage}
+                source={require('../assets/images/birthday.png')}
+              />
+              <TextInput style={styles.inputStyle} placeholder="Birthday" />
+            </View>
+          </View>
+          <View style={styles.signupS3Container}>
+            <Text style={styles.signupS3Text}>
+              By clicking below, you agree to Wattapads Terms Of Service {`&`}
+              Privacy Policy.
+            </Text>
+            <TouchableOpacity>
+              <Text>Start Reading</Text>
+            </TouchableOpacity>
+          </View>
+          {/* footer section */}
+          <Text>Have an account? Log in</Text>
+        </ScrollView>
       </View>
-      {/* sign up with fb & google section */}
-      <View style={styles.signupS2Container}>
-        <TouchableOpacity
-          style={styles.facebookSignupButton}>
-          <Image
-            style={styles.signupFacebookLogo}
-            source={require('../assets/images/facebook.png')}
-          />
-          <Text style={styles.SignupS2Text}>Facebook</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.googleSignupButton}>
-          <Image
-            style={styles.signupGoogleLogo}
-            source={require('../assets/images/google.png')}
-          />
-          <Text style={styles.SignupS2Text}>Google</Text>
-        </TouchableOpacity>
-      </View>
-      {/* signup form section */}
-      <View>
-        <View style={styles.emptyViewContainer}>
-          <View style={ styles.emptyView}></View>
-          <Text style={styles.emptyViewText}>OR</Text>
-          <View style={styles.emptyView}></View>
-        </View>
-      </View>
-      {/* footer section */}
     </View>
   );
 };
--- src/styling/styles.js
+++ src/styling/styles.js
@@ -10,8 +10,8 @@ const COLORS = {
   grey: '#949494',
   blue: '#3b5998',
 };
-const signupSecondaryButton = {
-  flex:1,
+const signupButton = {
+  flex: 1,
   flexDirection: 'row',
   justifyContent: 'center',
   alignItems: 'center',
@@ -26,8 +26,9 @@ const styles = StyleSheet.create({
   //signup
   signupContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignSelf: 'center'
+    alignItems: 'center',
+    justifyContent:'center'
+    
   },
   signupS1Container: {
     flexDirection: 'column',
@@ -35,10 +36,12 @@ const styles = StyleSheet.create({
   },
   signupLogo: {
     resizeMode: 'contain',
-    margin: 10,
+    marginBottom: 30,
+    width: 200,
+    height: 40,
   },
   signupLogoText: {
-    fontSize: 14,
+    fontSize: 12,
     fontFamily: 'Montserrat-LightItalic',
     letterSpacing: 2,
     color: COLORS.lightGrey,
@@ -49,16 +52,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   facebookSignupButton: {
-    ...signupSecondaryButton,
+    ...signupButton,
     backgroundColor: COLORS.blue,
     height: 42,
-    marginRight: 5
+    marginRight: 5,
   },
   googleSignupButton: {
-    ...signupSecondaryButton,
+    ...signupButton,
     backgroundColor: 'transparent',
     borderColor: COLORS.grey,
-    marginLeft: 5
+    marginLeft: 5,
   },
   signupFacebookLogo: {
     width: 40,
@@ -79,10 +82,10 @@ const styles = StyleSheet.create({
   emptyViewContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   emptyView: {
-    flex:1,
+    flex: 1,
     backgroundColor: COLORS.grey,
     height: 1,
   },
@@ -91,6 +94,54 @@ const styles = StyleSheet.create({
     paddingLeft: 5,
     paddingRight: 5,
   },
+  inputsContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: COLORS.grey,
+    borderRadius: 5,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderBottomColor: COLORS.grey,
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  inputContainer1: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  inputImage: {
+    width: 25,
+    height: 25,
+    marginRight: 5,
+  },
+  inputStyle: {
+    flex: 1,
+    fontFamily: 'Montserrat-Medium',
+    color: COLORS.white,
+  },
+  signupS3Container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signupS3Text: {
+    textAlign: 'center',
+    marginBottom: 15,
+    fontFamily: 'Montserrat-Medium',
+    color: COLORS.white,
+    fontSize: 12,
+  },
+  signupSubmitButton: {},
 });
 
 export default styles;
