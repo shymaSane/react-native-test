8dfc3446e96afd29150c23bb06eb58d083e47510
shymaSane <shymaSane1@gmail.com>
2021-09-22 11:33:53 +0300

    [fix]two section of singup screen

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import styles from '../styling/styles';
+import UserOrientations from '../assets/UserOrientations';
 
 const Signup = ({navigation}) => {
+  const WIDTH = UserOrientations().width;
+  const HEIGHT = UserOrientations().height;
+
   return (
-    <View style={styles.signupContainer}>
+    <View style={{width: .85*WIDTH ,...styles.signupContainer}}>
       {/* logo section */}
-      <View style={styles.signupLogoContainer}>
+      <View style={styles.signupS1Container}>
         <Image
           style={styles.signupLogo}
           source={require('../assets/images/logo-short.png')}
@@ -14,23 +18,32 @@ const Signup = ({navigation}) => {
         <Text style={styles.signupLogoText}>Where Stories Live</Text>
       </View>
       {/* sign up with fb & google section */}
-      <View style={styles.signupSecondaryContainer}>
-        <TouchableOpacity style={styles.facebookSignupButton}>
+      <View style={styles.signupS2Container}>
+        <TouchableOpacity
+          style={styles.facebookSignupButton}>
           <Image
-            style={styles.signupSecondaryFacebookLogo}
+            style={styles.signupFacebookLogo}
             source={require('../assets/images/facebook.png')}
           />
-          <Text style={styles.SignupSecondayText}>Facebook</Text>
+          <Text style={styles.SignupS2Text}>Facebook</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.googleSignupButton}>
+        <TouchableOpacity
+          style={styles.googleSignupButton}>
           <Image
-            style={styles.signupSecondaryGoogleLogo}
+            style={styles.signupGoogleLogo}
             source={require('../assets/images/google.png')}
           />
-          <Text style={styles.SignupSecondayText}>Google</Text>
+          <Text style={styles.SignupS2Text}>Google</Text>
         </TouchableOpacity>
       </View>
       {/* signup form section */}
+      <View>
+        <View style={styles.emptyViewContainer}>
+          <View style={ styles.emptyView}></View>
+          <Text style={styles.emptyViewText}>OR</Text>
+          <View style={styles.emptyView}></View>
+        </View>
+      </View>
       {/* footer section */}
     </View>
   );
--- src/styling/styles.js
+++ src/styling/styles.js
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-const colors = {
+const COLORS = {
   darkOrange: '#fe5009',
   midOrange: '#fe6121',
   lightOrange: '#fe723a',
@@ -11,17 +11,15 @@ const colors = {
   blue: '#3b5998',
 };
 const signupSecondaryButton = {
+  flex:1,
   flexDirection: 'row',
   justifyContent: 'center',
   alignItems: 'center',
-  width: 150,
   height: 40,
   borderWidth: 1,
   borderRadius: 5,
   marginTop: 20,
   marginBottom: 20,
-  marginRight: 5,
-  marginLeft: 5,
 };
 
 const styles = StyleSheet.create({
@@ -29,10 +27,9 @@ const styles = StyleSheet.create({
   signupContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'center'
   },
-  signupLogoContainer: {
-    width: '80%',
+  signupS1Container: {
     flexDirection: 'column',
     alignItems: 'center',
   },
@@ -44,41 +41,55 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontFamily: 'Montserrat-LightItalic',
     letterSpacing: 2,
-    color: colors.lightGrey,
+    color: COLORS.lightGrey,
   },
-  signupSecondaryContainer: {
-    width: '80%',
+  signupS2Container: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
   },
-
   facebookSignupButton: {
     ...signupSecondaryButton,
-    backgroundColor: colors.blue,
+    backgroundColor: COLORS.blue,
     height: 42,
-    width: 152,
+    marginRight: 5
   },
   googleSignupButton: {
     ...signupSecondaryButton,
     backgroundColor: 'transparent',
-    borderColor: colors.grey,
+    borderColor: COLORS.grey,
+    marginLeft: 5
   },
-  signupSecondaryFacebookLogo: {
+  signupFacebookLogo: {
     width: 40,
     height: 40,
     resizeMode: 'contain',
   },
-  signupSecondaryGoogleLogo: {
+  signupGoogleLogo: {
     width: 25,
     height: 25,
     resizeMode: 'contain',
   },
-  SignupSecondayText: {
+  SignupS2Text: {
     fontFamily: 'Montserrat-Medium',
     fontSize: 14,
     marginLeft: 10,
-    color: colors.white,
+    color: COLORS.white,
+  },
+  emptyViewContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyView: {
+    flex:1,
+    backgroundColor: COLORS.grey,
+    height: 1,
+  },
+  emptyViewText: {
+    color: COLORS.grey,
+    paddingLeft: 5,
+    paddingRight: 5,
   },
 });
 
